8a8852c84f655d72a0bd1d2497ef3c15c68bbbab
AkarisDimitry <jmlkombardi@live.com>
2022-06-28 10:01:17 -0300

    test

---
+++ files/dataset/dataset_catalisis.py
@@ -0,0 +1,36 @@
+Set = {
+	# **** **** CoTPyP_13.70_ **** **** #
+	# --- D3 --- #/home/busnengo/Juan.Manuel/cellOPT/sust.coordination/FeTPyPCo/M1TPyP/13.70cell/rmFREEMAG/rmCo/rmNUD3_i/rmmag
+	'CoTPyP_13.70__D3':{
+		'*'		:	'/home/busnengo/Juan.Manuel/cellOPT/sust.coordination/FeTPyPCo/M1TPyP/13.70cell/rmFREEMAG/rmCo/rmNUD3_i/rmmag/',
+		'*O2'	:	None,
+		'*O'	:	'/home/busnengo/Juan.Manuel/cellOPT/sust.coordination/FeTPyPCo/M1TPyP/13.70cell/O/rmXCHANGE.FROM.OPT/rmCo',
+		'*OH'	:	'/home/busnengo/Juan.Manuel/cellOPT/sust.coordination/FeTPyPCo/M1TPyP/13.70cell/OH/rmCo',
+		'*OOH'	:	'/home/busnengo/Juan.Manuel/cellOPT/sust.coordination/FeTPyPCo/M1TPyP/13.70cell/OOH/rmCo/rmMAG00',
+		'H2'	:	'/home/busnengo/Juan.Manuel/alone/H2',
+		'H2O'	:	'/home/busnengo/Juan.Manuel/alone/H2O',
+		'PDOS'	:	'/home/busnengo/Juan.Manuel/cellOPT/sust.coordination/FeTPyPCo/M1TPyP/13.70cell/rmFREEMAG/rmCo/rmNUD3_i/rmmag/rmPDOS',
+	},
+	# --- D3BJ --- #
+	'CoTPyP_13.70__D3BJ':{
+		'*'		:	'/home/busnengo/Juan.Manuel/cellOPT/sust.coordination/FeTPyPCo/M1TPyP/13.70cell/rmFREEMAG/rmCo/rmNUD3_i/rmmag/rmBJvdw',
+		'*O2'	:	None,
+		'*O'	:	'/home/busnengo/Juan.Manuel/cellOPT/sust.coordination/FeTPyPCo/M1TPyP/13.70cell/O/rmXCHANGE.FROM.OPT/rmCo/rmBJvdw',
+		'*OH'	:	'/home/busnengo/Juan.Manuel/cellOPT/sust.coordination/FeTPyPCo/M1TPyP/13.70cell/OH/rmCo/rmBJvdw',
+		'*OOH'	:	'/home/busnengo/Juan.Manuel/cellOPT/sust.coordination/FeTPyPCo/M1TPyP/13.70cell/OOH/rmCo/rmMAG00/rmBJvdw',
+		'H2'	:	'/home/busnengo/Juan.Manuel/alone/H2/rmBJvdw',
+		'H2O'	:	'/home/busnengo/Juan.Manuel/alone/H2O/rmBJvdw',
+		'PDOS'	:	'/home/busnengo/Juan.Manuel/cellOPT/sust.coordination/FeTPyPCo/M1TPyP/13.70cell/rmFREEMAG/rmCo/rmNUD3_i/rmmag/rmPDOS/rmBJvdw',
+	},
+	# --- 331 --- #
+	'CoTPyP_13.70__331':{
+		'*'		:	'/home/busnengo/Juan.Manuel/cellOPT/sust.coordination/FeTPyPCo/M1TPyP/13.70cell/rmFREEMAG/rmCo/rmNUD3_i/rmmag/rm331',
+		'*O2'	:	None,
+		'*O'	:	'/home/busnengo/Juan.Manuel/cellOPT/sust.coordination/FeTPyPCo/M1TPyP/13.70cell/O/rmXCHANGE.FROM.OPT/rmCo/rm331',
+		'*OH'	:	'/home/busnengo/Juan.Manuel/cellOPT/sust.coordination/FeTPyPCo/M1TPyP/13.70cell/OH/rmCo/rm331',
+		'*OOH'	:	'/home/busnengo/Juan.Manuel/cellOPT/sust.coordination/FeTPyPCo/M1TPyP/13.70cell/OOH/rmCo/rmMAG00/rm331',
+		'H2'	:	'/home/busnengo/Juan.Manuel/alone/H2',
+		'H2O'	:	'/home/busnengo/Juan.Manuel/alone/H2O',
+		'PDOS'	:	'/home/busnengo/Juan.Manuel/cellOPT/sust.coordination/FeTPyPCo/M1TPyP/13.70cell/rmFREEMAG/rmCo/rmNUD3_i/rmmag/rmPDOS',
+	},
+}
